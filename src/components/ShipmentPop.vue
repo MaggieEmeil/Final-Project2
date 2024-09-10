b423e3660daa591d5426575b3b16ec41572ed84b
MaggieEmeil <mangoooo2013@gmail.com>
2024-09-10 15:23:32 +0300

    change design

--- src/components/ShipmentPop.vue
+++ src/components/ShipmentPop.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class=" max-h-full fixed inset-0 backdrop-blur-3xl shadow-black shadow-md flex items-center justify-center z-50">
-    <div class="p-10 rounded-md bg-yellow-100 shadow-lg shadow-black hover:shadow-gray-200 sm:w-3/4  lg:w-2/5 md:w-3/4" @submit.prevent='input'>
-      <nav class=" border-gray-200 dark:bg-gray-900 mb-11">  
+  <div class=" max-h-full fixed inset-0 backdrop-blur-lg z-50 shadow-black shadow-md flex items-center justify-center">
+    <div class="p-10 rounded-md bg-yellow-100 shadow-lg shadow-black hover:shadow-black sm:w-3/4  lg:w-2/5 md:w-3/4" @submit.prevent='emitDone'>
+      <nav class=" border-gray-200 dark:bg-blue-900 mb-11">  
         <div class=" max-w-screen flex flex-wrap items-center justify-between mx-auto p-4">
           <span class="m-auto self-center text-2xl font-semibold text-center whitespace-nowrap dark:text-white">ADD SHOPPING ITEM</span>
             <button type="submit" @click="cancel" class="rounded-full mr-3">
@@ -15,49 +15,48 @@
       </nav>  
       <form class=" font-serif text-lg">
         <div class=" mt-6 flex items-center justify-between ">
-          <label class="font-semibold text-gray-800 text-md mb-2 block  ">Item Details</label>
-          <input name="item name" type="text"  class=" w-48  bg-white border border-gray-300 text-gray-800 text-sm px-4 py-2.5 rounded-md outline-blue-500" placeholder="Enter item name" v-model="ItemName" />
-          <span v-if="emailerror" class="text-red-500 text-sm">{{ nameerror }}</span>
+          <label class="font-semibold text-gray-800 text-md mb-2 block  ">Item Name</label>
+          <input name="item name" type="text"  class=" w-48  bg-white border  border-shadow-black text-gray-800 text-sm px-4 py-2.5 rounded-md outline-blue-500" placeholder="Enter item name" v-model="ItemName" />
+          <span v-if="nameerror" class="text-red-500 text-sm">{{ nameerror }}</span>
         </div>
         <div class="mt-6 flex items-center justify-between ">
           <label class="font-semibold  text-gray-800 text-md mb-2 block">Item Quantity</label>
           <div class="bg-blue-200  flex items-center text-center rounded-full ">
             <button @click="dec" type="button"  class="flex items-center justify-center  align-center rounded-full text-white bg-gradient-to-r from-sky-900 to-sky-600  p-3 h-8 "><span class="text-md font-lg text-center font-bold ">-</span></button>
-            <input type="text" class=" text-center rounded-full bg-blue-200 border-x-0 border-gray-300 h-8 font-medium  text-gray-900 w-32 text-md "  required  v-model="number" >
+            <input type="text" class=" text-center rounded-full bg-blue-200 border-x-0 border-gray-300 h-8 font-bold  text-gray-900 w-32 text-md "  required  v-model="number" >
             <button @click="inc" type="button"  class="flex items-center justify-center align-center rounded-full text-white bg-gradient-to-r from-sky-900 to-sky-600  p-3 h-8 "><span class="text-md font-lg  font-bold  text-center flex justify-center pb-1">+</span></button>
           </div>
-          <span v-if="emailerror" class="text-red-500 text-sm">{{ priceerror }}</span>
+          <span v-if="numbererror" class="text-red-500 text-sm">{{ numbererror }}</span>
         </div>
         <div class="mt-6  flex items-center justify-between  ">
           <label class="font-semibold text-gray-800 text-md mb-2 block ">Total({{ price }}) $</label>
-          <input name="price" type="text" class="bg-white border border-gray-300 text-gray-800 text-sm px-4 py-2.5 rounded-md outline-blue-500 w-48" placeholder="Single Item Price $ " v-model="price" />
-          <span v-if="emailerror" class="text-red-500 text-sm">{{ kiloerror }}</span>
+          <input name="price" type="number" class="bg-white border border-gray-300 text-gray-800 text-sm px-4 py-2.5 rounded-md outline-blue-500 w-48" placeholder="Single Item Price $ " v-model="price" />
+          <span v-if="priceerror" class="text-red-500 text-sm">{{ priceerror }}</span>
         </div>
         <div class="mt-6  flex items-center justify-between">
           <label class="font-semibold text-gray-800 text-md mb-2 block ">Total({{kilo}}) Kg</label>
           <input name="item name" type="number"  class="w-48 items-center bg-white border border-gray-300 text-gray-800 text-sm px-4 py-2.5 rounded-md outline-blue-500" placeholder="Single Item Weight" v-model="kilo" />
-          <span v-if="emailerror" class="text-red-500 text-sm">{{ numbererror }}</span> 
+          <span v-if="kiloerror" class="text-red-500 text-sm">{{ kiloerror }}</span> 
         </div>
-        <div class=" flex items-center  justify-center mt-7">
+        <div class=" flex items-center  justify-between mt-7">
           <label class="font-semibold text-gray-800 text-md  block">Item Photo :</label> 
-          <div class="flex items-center justify-center border-lined border-4  rounded border-sky-800 w-20 ">
+          <div class="flex items-center justify-center border-lined border-4  rounded border-sky-800 w-48 ">
             <input
               type="file"
               @change="onFileChange"
               className="
-              text-sm text-white file:rounded-md file:w-44  file:h-10
+              text-sm text-white file:rounded-md file:w-48  file:h-10
               file:py-1 file:px-3 file:border-none
               file:font-medium
               file:bg-gradient-to-tr from-sky-700 to-sky-400 file:text-white
               hover:file:cursor-pointer 
               hover:file:text-black "
-            />
-            <span v-if="emailerror" class="text-red-500 text-sm">{{ emailerror }}</span> 
+            /> 
           </div>
         </div>
         <br>
-        <div class="flex items-center justify-around mt-5">
-          <button type="submit" @click="cancel" class="w-1/4    py-3 px-4 font-semibold text-md rounded-full text-white bg-gradient-to-r from-sky-900 to-sky-600 hover:bg-gray-800">
+        <div class="flex items-center justify-center gap-5 mt-5">
+          <button type="button" @click="cancel" class="w-1/4    py-3 px-4 font-semibold text-md rounded-full text-white bg-gradient-to-r from-sky-900 to-sky-600 hover:bg-gray-800">
             CANCEL
           </button>
           <button type="submit" @click="emitDone" class=" w-1/4    py-3 px-4 font-semibold text-md rounded-full text-white bg-gradient-to-r from-sky-900 to-sky-600 hover:bg-gray-800">
@@ -86,19 +85,19 @@ const numbererror = ref('');
 const dec = () => { if (number.value> 1) number.value--; };
 const inc = () => { number.value ++; };
 const cancel=()=>{
-  router.push({ name: 'ShipmentsPage' });
+  router.push({ name: 'shipmentPage' });
 }
 const validateForm = () => {
   if (!ItemName.value) {
       nameerror.value = 'Please enter the item name';
     return false;
   }
-  else if (!price.value || price.value < 0) {
-    priceerror.value = 'Please enter the item price';
+  else if (!price.value || price.value <= 0 ) {
+    priceerror.value = 'Please enter the item price,it should be a positive number';
     return false;
   }
-  else if (!kilo.value || kilo.value < 0) {
-    kiloerror.value = 'Please enter the item weight ';
+  else if (!kilo.value || kilo.value <= 0) {
+    kiloerror.value = 'Please enter the item weight,it should be a positive number ';
     return false;
   }
    
@@ -110,7 +109,7 @@ const emitDone = () => {
     emit('done', itemData.value={ 
        itemName: ItemName.value, 
       quantity: number.value, 
-      Price: price.value,  
+      price: price.value, 
       weight: kilo.value,       
     });
   }
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -7,27 +7,9 @@
         '-translate-x-full': !isSidebarOpen,
         'sm:translate-x-0': true,
       }"
-      class="w-64 h-[100vh] min-h-full transition-transform bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+      class="fixed top-20 w-64 max-h-[100vh] p-5 min-h-full transition-transform border-t border-r border-white dark:bg-sky-900 "
     >
       <router-link to="" class="flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-8 me-3"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <g
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path
-              d="M10.5 19.5L21 8.6l-7.5 2L5.4 3m0 0l1.8 7.6-2 7.5-3.2-1L3 5.4z"
-            />
-          </g>
-        </svg>
         <span
           id="logo2"
           class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white"
@@ -65,7 +47,7 @@
               >Trips</router-link
             >
           </li>
-          <li>
+          <!-- <li>
             <router-link
               :to="{
                 name: 'shipmentPage',
@@ -73,7 +55,7 @@
               class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >Profile</router-link
             >
-          </li>
+          </li> -->
         </ul>
       </div>
     </aside>
