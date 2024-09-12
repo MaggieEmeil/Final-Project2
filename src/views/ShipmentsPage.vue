e52e80def1342a4f13a27287a6bbaeaf72faf771
Andrew Melad <andrewmelad108@gmail.com>
2024-09-12 14:20:28 +0300

    meged with main

--- src/views/ShipmentsPage.vue
+++ src/views/ShipmentsPage.vue
@@ -1,27 +1,22 @@
 <template>
-  
-    <div class="bg-gradient-to-r from-sky-200 to-sky-900  h-full flex flex-wrap shadow-lg rounded-lg  w-full " @submit.prevent="input">
-      <Card  @click="display" @close='close'> </Card>   
-    </div>
-</template> 
+  <div
+    class="bg-gradient-to-r from-sky-200 to-sky-900 h-full flex flex-wrap shadow-lg rounded-lg w-full"
+    @submit.prevent="input"
+  >
+    <Card @click="display" @close="close"> </Card>
+  </div>
+</template>
 <script setup>
-import { ref } from 'vue';
- import {useRouter} from 'vue-router';
- import Card from '../components/Card.vue';
-
-const arr=ref(['l']);
-const shipmentData = ref({from:'',
-      to: '',
-      date: '',
-      name:'',
-      items: ''}); 
-const router=useRouter();
-const btn=()=>{
-      router.push({ name: 'ShipmentsInfo' });}
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Card from "../components/Card.vue";
+const arr = ref(["l"]);
+const shipmentData = ref({ from: "", to: "", date: "", name: "", items: "" });
+const router = useRouter();
+const btn = () => {
+  router.push({ name: "ShipmentsInfo" });
+};
 // const addMore=()=>{
 //      router.push({ name: 'ShipmentsInfo' });}
 </script>
-<style scoped>
-
-</style>
-
+<style scoped></style>
